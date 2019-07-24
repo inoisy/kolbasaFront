c687e1bf32280943692bc394af72c8fe57eff66b
inoisy <inoisy@bk.ru>
2019-07-24 14:45:06 +0300

    fix

--- components/Busket.vue
+++ components/Busket.vue
@@ -10,7 +10,12 @@
         </div>
       </v-toolbar>
       <div class="px-4 py-5">
-        <!-- <v-data-table
+        <contact-form />
+        <!-- <v-btn color="accent">Оформить</v-btn> -->
+      </div>
+    </div>
+
+    <!-- <v-data-table
           :items="basket"
           :disable-pagination="true"
           :headers="[{ text: 'id', sortable: false, value: 'id' },{ text: 'name', sortable: true, value: 'name' }]"
@@ -19,83 +24,64 @@
             <td class>{{ props.item.id}}</td>
             <td class>{{ props.item.name}}</td>
           </template>
-        </v-data-table>-->
-        <!-- <h2>Заполните данные</h2> -->
-        <contact-form />
-        <!-- <v-btn color="accent">Оформить</v-btn> -->
-      </div>
-    </div>
-
+    </v-data-table>-->
+    <!-- <h2>Заполните данные</h2> -->
     <div v-show="!offer" class="mb-4">
       <v-toolbar class="grey lighten-2 px-2" height="100px">
         <div class="toolbar-inner">
           <h2 class="mb-0 display-3 mont font-weight-bold">Корзина</h2>
-          <!-- <v-btn
-            class="close-btn ma-0 ml-auto display-flex"
-            outline
-            icon
-            large
-            @click="$parent.basketDrawer=false"
-          >
-            <v-icon>close</v-icon>
-          </v-btn>-->
         </div>
       </v-toolbar>
       <div class="pa-4">
-        <v-flex xs12 v-for="(item,index) in basket" :key="item.id" class="mb-1">
-          <!---->
-          <v-layout row>
-            <div
-              class="img-wrapper display-flex gray xs2 flex pr-3 hidden-xs-only"
-              style="min-width:100px"
-            >
-              <img
-                class="d-block ma-auto"
-                style="height:80px; object-fit: contain;"
-                v-lazy="item.img ? imageBaseUrl+item.img.url : require('~/assets/no-image.png')"
-                :alt="item.name"
-              />
-            </div>
-            <v-flex class="d-flex">
-              <nuxt-link
-                :to="`/catalog/${item.category.slug}/${item.slug}`"
-                class="display-1 mont d-block my-auto"
-              >{{item.name}}</nuxt-link>
-            </v-flex>
-            <v-flex style="display:flex; flex-direction:row;">
-              <v-text-field
-                class="ma-auto mr-4"
-                style="width:150px"
-                type="number"
-                label="Количество"
-                append-outer-icon="add"
-                @click:append-outer="increment($event,item.id)"
-                prepend-icon="remove"
-                @click:prepend="decrement($event,item.id)"
-                @change="handleQuantityChange($event,item.id)"
-                :value="item.count"
-                hide-details
-              ></v-text-field>
+        <!-- <div v-for="(item,index) in basket" :key="index" class="mb-1 layout row">
+          <div
+            class="img-wrapper display-flex gray xs2 flex pr-3 hidden-xs-only"
+            style="min-width:100px"
+          >
+            <img
+              class="d-block ma-auto"
+              style="height:80px; object-fit: contain;"
+              v-lazy="item.img ? imageBaseUrl+item.img.url : require('~/assets/no-image.png')"
+              :alt="item.name"
+            />
+          </div>
+          <v-flex class="d-flex">
+            <nuxt-link
+              :to="`/catalog/${item.category.slug}/${item.slug}`"
+              class="display-1 mont d-block my-auto"
+            >{{item.name}}</nuxt-link>
+          </v-flex>
+          <v-flex style="display:flex; flex-direction:row;">
+            <v-text-field
+              class="ma-auto mr-4"
+              style="width:150px"
+              type="number"
+              label="Количество"
+              append-outer-icon="add"
+              @click:append-outer="increment($event,item.id)"
+              prepend-icon="remove"
+              @click:prepend="decrement($event,item.id)"
+              @change="handleQuantityChange($event,item.id)"
+              :value="item.count"
+              hide-details
+            ></v-text-field>
 
-              <v-btn
-                class="ma-auto"
-                color="error"
-                icon
-                outline
-                @click="$store.commit('removeFromBasket',item.id)"
-              >
-                <v-icon>delete_outline</v-icon>
-              </v-btn>
-            </v-flex>
-          </v-layout>
-          <v-divider class="my-4"></v-divider>
-          <!-- v-if="index!==basket.length-1" -->
-        </v-flex>
-        <v-flex xs12 class="mt-4" v-show="!offer">
-          <v-btn color="accent" class="ml-0" large @click="offer=true">Оформить заказ</v-btn>
+            <v-btn
+              class="ma-auto"
+              color="error"
+              icon
+              outline
+              @click="$store.commit('removeFromBasket',item.id)"
+            >
+              <v-icon>delete_outline</v-icon>
+            </v-btn>
+            <v-divider class="my-4"></v-divider>
+          </v-flex>
+        </div>-->
+        <v-flex xs12 class="mt-4">
+          <v-btn color="accent" class="ml-0" large @click="handleOfferClick">Оформить заказ</v-btn>
         </v-flex>
       </div>
-      <!-- <v-divider class="my-4"></v-divider> -->
     </div>
   </div>
 </template>
@@ -114,10 +100,14 @@ export default {
   data() {
     return {
       imageBaseUrl: process.env.imageBaseUrl,
-      offer: false
+      offer: false,
+      basket: this.$store.state.localStorage.basket
     };
   },
   methods: {
+    handleOfferClick() {
+      this.offer = true;
+    },
     async handleQuantityChange(val, id) {
       console.log("TCL: handleQuantityChange -> id", id);
       // console.log("TCL: handleQuantityChange -> val1", val1);
@@ -139,9 +129,9 @@ export default {
     // contacts() {
     //   return this.$store.state.generalInfo.contacts;
     // }
-    basket() {
-      return this.$store.state.localStorage.basket;
-    }
+    // basket() {
+    //   return this.$store.state.localStorage.basket;
+    // }
   }
 };
 </script>
