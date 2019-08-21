b647afa6fa86fd85ae9e356ac33e205f0dd67aad
inoisy <inoisy@bk.ru>
2019-08-21 15:38:03 +0300

    add phone number

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -5,32 +5,45 @@
     <section class="background" v-lazy:background-image="require('~/assets/img/bg.jpg')">
       <v-container class="py-5">
         <v-layout row wrap class="mont">
-          <!-- {{contacts}} -->
-          <div class="flex xs12 mb-3" data-aos="fade-up">
-            <a :href="`tel:${contacts.phone}`" class="link font-weight-medium display-1">
+          <div class="flex xs12 mb-3 align-center display-flex" data-aos="fade-up">
+            <a
+              :href="`tel:${contacts.phone}`"
+              class="link font-weight-medium display-1 d-inline-flex"
+            >
               <v-icon class="mr-3" color="rgba(0,0,0,0.87)">phone</v-icon>
               {{contacts.phone}}
             </a>
+            <span>,&nbsp;&nbsp;</span>
+            <a
+              :href="`tel:+79261191748`"
+              class="link font-weight-medium display-1 d-inline-flex"
+              style="min-height:32px"
+            >+7 (926) 119 17 48 (оптовый отдел)</a>
           </div>
           <div class="flex xs12 mb-3" data-aos="fade-up">
-            <a :href="`mailto:${contacts.email}`" class="link font-weight-medium display-1">
+            <a
+              :href="`mailto:${contacts.email}`"
+              class="link font-weight-medium display-1 d-inline-flex"
+            >
               <v-icon class="mr-3" color="rgba(0,0,0,0.87)">mail</v-icon>
               {{contacts.email}}
             </a>
           </div>
           <div class="flex xs12 mb-3" data-aos="fade-up">
-            <a @click="$vuetify.goTo('#map')" class="link font-weight-medium display-1">
+            <a
+              @click="$vuetify.goTo('#map')"
+              class="link font-weight-medium display-1 d-inline-flex"
+            >
               <v-icon class="mr-3" color="rgba(0,0,0,0.87)">location_on</v-icon>
               {{contacts.addressText}}
             </a>
           </div>
           <div class="flex xs12 mb-5" data-aos="fade-up">
-            <div class="font-weight-medium display-1">
+            <div class="font-weight-medium display-1 d-inline-flex">
               <v-icon class="mr-3" color="rgba(0,0,0,0.87)">access_time</v-icon>
               {{contacts.accessTime}}
             </div>
           </div>
-          <!-- <div data-aos="fade-up"> -->
           <no-ssr>
             <yandex-map
               id="map"
@@ -42,7 +55,6 @@
               <ymap-marker marker-id="1" marker-type="placemark" :coords="contacts.addressCoords"></ymap-marker>
             </yandex-map>
           </no-ssr>
-          <!-- </div> -->
         </v-layout>
       </v-container>
     </section>
