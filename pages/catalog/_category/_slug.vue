db1488b25846e641a7547c74617b5d7a77c590e9
inoisy <inoisy@bk.ru>
2020-03-06 23:25:30 +0300

    update

--- pages/catalog/_category/_slug.vue
+++ pages/catalog/_category/_slug.vue
@@ -19,64 +19,80 @@
 import ProductDialog from "~/components/ProductDialog";
 export default {
   head() {
-    return {
-      title: this.isProduct
-        ? this.product && this.product.name
-          ? `${this.product.name} оптом`
-          : ""
-        : this.category && this.category.name
-        ? `${this.category.name} оптом`
-        : "",
-      link: [
-        {
-          rel: "canonical",
-          href: this.isProduct
-            ? this.category &&
-              this.category.slug &&
-              this.product &&
-              this.product.slug
-              ? `https://prodaem-kolbasu.ru/catalog/${this.category.slug}/${this.product.slug}`
-              : ""
-            : this.category && this.category.slug
-            ? `https://prodaem-kolbasu.ru/catalog/${this.category.slug}`
-            : ""
-        }
-      ],
-      meta: [
-        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
-        {
-          hid: "description",
-          name: "description",
-          content: this.isProduct
-            ? this.product && this.product.description
+    let head;
+    if (this.isProduct) {
+      if (!this.product || !this.product.name) return;
+      head = {
+        title: `${this.product.name} оптом`,
+        link: [
+          {
+            rel: "canonical",
+            href:
+              this.category && this.category.slug
+                ? `https://prodaem-kolbasu.ru/catalog/${this.category.slug}/${this.product.slug}`
+                : ""
+          }
+        ],
+        meta: [
+          // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+          {
+            hid: "description",
+            name: "description",
+            content: this.product.description
               ? this.product.description.slice(0, 310)
               : `${this.product.name} оптом. ${this.product.name} от компании Альянс Фуд с доставкой по всей РФ и СНГ по самым выгодным оптовым ценам от производителя.`
-            : this.category && this.category.description
-            ? this.category.description.slice(0, 310)
-            : `${this.category.name} оптом. ${this.category.name} от компании Альянс Фуд с доставкой по всей РФ и СНГ по самым выгодным оптовым ценам от производителя.`
-        }
-      ]
-    };
+          }
+        ]
+      };
+      // console.log("head -> head", head);
+      return head;
+    } else {
+      if (!this.category || !this.category.name) return;
+      head = {
+        title: `${this.category.name} оптом`,
+        link: [
+          {
+            rel: "canonical",
+            href: `https://prodaem-kolbasu.ru/catalog/${this.category.slug}`
+          }
+        ],
+        meta: [
+          {
+            hid: "description",
+            name: "description",
+            content: this.category.description
+              ? this.category.description.slice(0, 310)
+              : `${this.category.name} оптом. ${this.category.name} от компании Альянс Фуд с доставкой по всей РФ и СНГ по самым выгодным оптовым ценам от производителя.`
+          }
+        ]
+      };
+      // console.log("head -> head", head);
+      return head;
+    }
+    
   },
   components: {
     ProductDialog
   },
   async asyncData(ctx) {
     let product = {};
-    if (ctx.params && ctx.params.slug) {
-      await ctx.store.dispatch("fetchGeneralInfo");
-      product = await ctx.store.dispatch("fetchProduct", {
-        slug: ctx.params.slug
+    if (ctx.params && !ctx.params.slug) return { product };
+
+    // if (ctx.params && ctx.params.slug) {
+    // await ctx.store.dispatch("fetchGeneralInfo");
+    product = await ctx.store.dispatch("fetchProduct", {
+      slug: ctx.params.slug,
+      type: "catalog"
+    });
+    if (!product) {
+      return ctx.error({
+        statusCode: 404,
+        message: "Продукт не найден"
       });
-      if (!product) {
-        return ctx.error({
-          statusCode: 404,
-          message: "Продукт не найден"
-        });
-      }
     }
+    // }
     return {
-      product: product
+      product
     };
   },
   watch: {
